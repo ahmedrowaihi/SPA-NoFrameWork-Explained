83184ed247caf94fdfa2bdeea476f840a26634e6
Ahmed Rowaihi <67356781+ahmedrowaihi@users.noreply.github.com>
2022-04-06 21:50:10 +0300

    Simple SPA - With No Frameworks!

---
+++ router.js
@@ -0,0 +1,35 @@
+const routes = {
+  404: "/views/404.html",
+  "/": "/views/home.html",
+  "/about": "/views/about.html",
+};
+// create an object to hold the routes
+
+const route = (event) => {
+  event = event || window.event;
+  //  get the event object
+  event.preventDefault();
+  //  prevent default behaviour
+  window.history.pushState({}, "", event.target.href);
+  //   change the url to the target href
+  handleLocation();
+  //   call handleLocation function
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  //   get current path name
+  const route = routes[path] || routes[404];
+  //   get the route from the routes object or the 404 route
+  const html = await fetch(route).then((data) => data.text());
+  // fetch current path view html
+  document.getElementById("view_container").innerHTML = html;
+  //   change view_container html to the fetched view html
+};
+
+window.onpopstate = handleLocation;
+//   listen for popstate event
+window.route = route;
+
+handleLocation();
+// call handleLocation function
